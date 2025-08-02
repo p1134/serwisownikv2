831d2a4284ca06555860b941f98231f249e873e7
Paulina <paulinarygula5@gmail.com>
2025-08-02 19:29:36 +0200

    fly.io deploy attemption

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -13,10 +11,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }},
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000',
     },
   },
 })
